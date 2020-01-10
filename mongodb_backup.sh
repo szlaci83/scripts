2dceb2a55b9ff8ffc87d15c08ac12ee19926386f
laszloglobal <laszlo.szoboszlai@global.com>
2020-01-10 13:44:22 +0000

    added param ooption

--- mongodb_backup.sh
+++ mongodb_backup.sh
@@ -1,16 +1,37 @@
 #!/bin/bash
-d=$(date +'%Y-%m-%d')
-echo $d
-mongodump --out /home/laci/backup/mongo
-tar -zcvf /home/laci/backup/mongodb/$d.tar.gz /home/laci/backup/mongo/
-rm -rf /home/laci/backup/mongo/
-#HOST=triton
-#USER=laci
 
-#ftp -inv $HOST << EOF
-#user $USER $PASS
-#cd /Backups/MongoDB
-#binary
-#mput mongodb.tar.gz
-#bye
-#EOF 
+# ad-hoc run should be done with a param, otherwise date is he suffix
+if [ $# -eq 0 ]
+then
+  d=$(date +'%Y-%m-%d')
+else
+  d = $1
+fi
+
+dump() {
+ mongodump --out /home/laci/backup/mongo;
+}
+
+zipit(){
+ tar -zcvf /home/laci/backup/mongodb/$d.tar.gz /home/laci/backup/mongo/
+}
+
+cleanup(){
+ rm -rf /home/laci/backup/mongo/
+}
+
+copy_to_ftp(){
+ HOST=triton
+ USER=laci
+ # ftp -inv $HOST << EOF
+ #user $USER $PASS
+ #cd /Backups/MongoDB
+ #binary
+ #mput mongodb.tar.gz
+ #bye
+ #EOF
+}
+
+dump
+zipit
+cleanup
