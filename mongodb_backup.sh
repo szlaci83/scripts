410ef110b7248c9c15aa47755b1db632af60a33e
laci <szlaci83@gmail.com>
2020-01-10 13:21:46 +0000

    added date

--- mongodb_backup.sh
+++ mongodb_backup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
+d=$(date +'%Y-%m-%d')
+echo $d
 mongodump --out /home/laci/backup/mongo
-tar -zcvf /home/laci/backup/mongodb.tar.gz /home/laci/backup/mongo/
+tar -zcvf /home/laci/backup/mongodb/$d.tar.gz /home/laci/backup/mongo/
 rm -rf /home/laci/backup/mongo/
 #HOST=triton
 #USER=laci
